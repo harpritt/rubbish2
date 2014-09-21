339708f2bfd95eaecf41b14fb03e361602fa5c9c
Harpritt <harpritt@gmail.com>
2014-09-21 23:36:12 +0100

    sdfads

--- js/abs_analytics.js
+++ js/abs_analytics.js
@@ -4,7 +4,3 @@ m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
 })(window,document,'script','/js/analytics.js','ga');
 
 
-ga('create', 'UA-55024156-1', {
-	'storage': 'none',
-	'clientId':'92bf24a5-20e5-4181-9778-2835f28c52d8'
-});
